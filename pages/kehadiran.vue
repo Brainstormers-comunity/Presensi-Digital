95917ff25de3b730a3c0a76058b3f5a9f4648500
ginarahma <141693381+Ginasrrhmwti@users.noreply.github.com>
2024-09-25 10:18:35 +0700

    Add files via upload

--- pages/kehadiran.vue
+++ pages/kehadiran.vue
@@ -2,14 +2,23 @@
     <div class="row">
         <div class="col">
             <h1 class="text-center my-3">DAFTAR KEHADIRAN </h1>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-11">
             <select @change="getSiswa" class="form-control form-control-lg form-select mb-3" style="background-color: #D9D9D9;">
                 <option value="" disabled>Pilih Tingkat</option>
                 <option v-for="k in kelas" :key="k.id" :value="k.id">{{ k.kelas }}</option>
             </select>
+
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-11">
             <table class="table table-striped table-bordered">
                 <thead>
                     <tr>
-                        <th rowspan="2">NAMA</th>
+                        <th rowspan="2">Nama Siswa</th>
                         <th colspan="5">KETERANGAN</th>
                     </tr>
                     <tr>
@@ -23,6 +32,7 @@
                 <tbody>
                     <tr v-for="(s, i) in siswa" :key="i">
                         <td>{{ s.nama }}</td>
+                        
                         <td><input v-model="presensi[i]" type="radio" :name="`keterangan[${i}]`" value="H" checked></td>
                         <td><input type="radio" :name="`keterangan[${i}]`" value="I"></td>
                         <td><input type="radio" :name="`keterangan[${i}]`" value="S"></td>
@@ -31,8 +41,10 @@
                     </tr>
                 </tbody>
             </table>
-            <button-right type="submit" class="btn btn-light btn-lg rounded-5 px-5" style=" text-decoration-color: #2E4D71;">KIRIM</button-right>
         </div>
+        <div class="col-2 ms-auto mb-3">
+            <button type="submit" class=" btn btn-light btn-lg rounded-5 px-5" style=" text-decoration-color: #2E4D71;">KIRIM</button>
+        </div>       
     </div>
     
 </template> 
---
+++ pages/laporan.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <h1 class="text-center my-3"> LAPORAN RIWAYAT KEHADIRAN </h1> 
+            <input type="text" class="rounded-5 mb-5 ps-3" placeholder="filter" style="width: 120rem;height:3rem;background-color: #D9D9D9; ">
+             </div>
+     <div class="row justify-content-center">
+            <div class="col-11">
+             <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th class="text-center" rowspan ="2">Tanggal</th>
+                        <th class="text-center" rowspan="2">Kelas</th>
+                        <th class="text-center" rowspan="2">Jumlah Siswa</th>
+                        <th class="text-center" colspan="5">KETERANGAN</th>
+                    </tr>
+                    <tr>
+                        <th>H</th>
+                        <th>I</th>
+                        <th>S</th>
+                        <th>A</th>
+                        <th>D</th>
+                    </tr>
+
+                    <tr>
+                        <td>25 SEPTEMBER 2024</td>
+                        <td>XI TOI</td>
+                        <td>33</td>
+                        <td>30</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>0</td>
+                    </tr>
+                </thead>
+             </table>
+         </div>
+         </div>
+         </div>
+</template>
